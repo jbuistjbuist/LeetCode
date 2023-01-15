f4c04fdca93ce7172c3e93b05687de410b8f355b
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2023-01-14 20:35:35 -0500

    Solve merge sorted linked list, non-recursive

---
+++ 21.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+//not recursive solution 
+
+var mergeTwoLists = function(list1, list2) {
+
+    if (!list1 || !list2) {
+        return list1 || list2
+    }
+    
+    const dummy = new ListNode(undefined, undefined);
+    let prev = dummy;
+
+    while(true) {
+
+        if (!list1 && !list2) {
+            break;
+        }
+
+        if (!list1) {
+            prev.next = list2;
+            break;
+        }
+        if (!list2) {
+            prev.next = list1;
+            break;
+        }
+
+        if (list1.val <= list2.val) {
+            prev.next = list1;
+            list1 = list1.next;
+        } else  {
+            prev.next = list2;
+            list2 = list2.next;
+        }
+        prev = prev.next;
+    }
+
+    return dummy.next;
+    
+};
